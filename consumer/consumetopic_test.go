7463aff94d6534341131a0697d89aa8e5a4f84a3
liuwenlong <liuwenlong2020@qq.com>
2025-06-02 23:19:20 +0800

    consumer group

--- consumer/consumetopic_test.go
+++ consumer/consumetopic_test.go
@@ -2,10 +2,24 @@ package consumer
 
 import (
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
 func TestConsumeTopic(t *testing.T) {
 	err := ConsumeTopic([]string{"192.168.1.11:9092"}, []string{"first"}, "first-test-1748858392")
 	assert.Equal(t, nil, err)
+}
+
+func TestConsumeTopic1(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+			err := ConsumeTopic([]string{"192.168.1.11:9092"}, []string{"first"}, "first-test-1748858392")
+			assert.Equal(t, nil, err)
+		}()
+	}
+	wg.Wait()
 }
